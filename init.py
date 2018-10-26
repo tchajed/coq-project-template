bf21f1560bfa6d1f1ef27e45217d31e035bd01e2
Tej Chajed <tchajed@mit.edu>
2018-10-25 22:13:55 -0400

    Replace bash init script with Python version
    Just so camelcase conversion is robust...

---
+++ init.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+from os import path
+import shutil
+
+script_dir = path.dirname(path.abspath(__file__))
+
+shutil.copy(path.join(script_dir, "Makefile"), os.curdir)
+
+if not path.exists(".gitignore"):
+    shutil.copy(path.join(script_dir, ".gitignore"), os.curdir)
+
+if not path.isdir("src"):
+    os.mkdir("src")
+if not path.isdir("vendor"):
+    os.mkdir("vendor")
+
+if not path.exists("vendor/.gitignore"):
+    shutil.copy(path.join(script_dir, "vendor", ".gitignore"), "vendor/")
+
+def to_camel_case(s):
+    """Convert s, with words separated by -, into CamelCase."""
+    words = s.split("-")
+    return "".join([word.capitalize() for word in words])
+
+if not path.exists("libname"):
+    dirname = path.basename(os.getcwd())
+    libname = to_camel_case(dirname.lstrip("coq-"))
+    with open("libname", "w") as f:
+        f.write(libname + "\n")
